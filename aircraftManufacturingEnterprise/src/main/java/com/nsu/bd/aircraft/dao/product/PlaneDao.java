2a555cbc35dea6e848f31a508f6f6272ffe9bd76
meg <m.khlybova@g.nsu.ru>
2020-06-04 18:03:52 +0700

    add second request to plane dao

--- aircraftManufacturingEnterprise/src/main/java/com/nsu/bd/aircraft/dao/product/PlaneDao.java
+++ aircraftManufacturingEnterprise/src/main/java/com/nsu/bd/aircraft/dao/product/PlaneDao.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
@@ -23,4 +24,17 @@ public interface PlaneDao extends CrudRepository<Plane, Integer> {
             "left join products on plane.id = products.id " +
             "where guild_id = :guildId", nativeQuery = true)
     List<String> findTypesByGuildId(@Param("guildId") int guildId);
+
+
+    @Query(value = "select plane.id, plane.engine_amount, plane.type from plane\n" +
+            "        left join products on plane.id = products.id\n" +
+            "        left join guild on guild.id = products.id\n" +
+            "    left join product_accounting on product_accounting.product_id = plane.id\n" +
+            "    left join company on company.id = guild.company_id\n" +
+            "where (begin_time, end_time) OVERLAPS ( :beginDate , :endDate )\n" +
+            "  and company.id = :companyId;",
+    nativeQuery = true)
+    List<Plane> findIdsByDateIntervalAndCompany(@Param("companyId") int companyId,
+                                                  @Param("beginDate") Date beginDate,
+                                                  @Param("endDate") Date endDate);
 }
